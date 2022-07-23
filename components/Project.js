74bdf4116ad5791c87f27f650c5deac4dc64fb11
dylanjohn <dylan@mediamachine.co.za>
2022-07-23 16:29:47 +0200

    Add swiper

--- components/Project.js
+++ components/Project.js
@@ -25,20 +25,6 @@ const Project = ({ id, name, tech, url, imgs, classes}) => {
     autoplay: {
       delay: 9000
     },
-    // on: {
-    //     init: function () {
-    //         if (this.slides.length <= 3) {
-    //             // First way:
-    //             this.allowSlidePrev = this.allowSlideNext = false; // disabling swiping
-    //             this.el.querySelector(".swiper-button-prev").setAttribute('hidden', '');  // hiding arrows prev&next
-    //             this.el.querySelector(".swiper-button-next").setAttribute('hidden', '');
-    //             this.el.querySelector(".swiper-pagination").setAttribute('hidden', '');
-    //             console.log(this);                // Second way:
-    //             // this.el.classList.add('swiper-no-swiping');
-    //         }
-    //     }
-    // },
-    // navigation,
     pagination: {
       clickable: true 
     }
@@ -46,13 +32,6 @@ const Project = ({ id, name, tech, url, imgs, classes}) => {
 
   return (
     <div>
-    {/*  <VisibilitySensor onChange={onChange} partialVisibility={true} active={active} >
-      </VisibilitySensor>*/}
-
-     {/* <Link href={`/projects/[id]`} as={`/projects/${id}`} passHref>
-         {name}
-      </Link>*/}
-
         <Swiper 
           //{...params}
           modules={[Navigation, Pagination, Scrollbar, A11y]}
@@ -76,32 +55,10 @@ const Project = ({ id, name, tech, url, imgs, classes}) => {
             </SwiperSlide>
           )}
         </Swiper>
-  
 
-     {/* {imgs.map((img, index) =>
-            <img
-              src={img.url}
-              alt={name}
-            />
-        )}*/}
-
-     {/* <Swiper {...params}>
-        {imgs.map((img, index) => 
-          <div key={img.id}>
-            <img
-              src={img.url}
-              alt={name}
-            />
-          </div>
-        )}
-      </Swiper>*/}
-
-
-        <div className="">
-          <h3>
-            <a href="#">{name}</a>
-          </h3>
-        </div>
+        <h3>
+          <a href="#">{name}</a>
+        </h3>
     </div>
   );
 }
